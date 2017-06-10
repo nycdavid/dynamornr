4b35ebe81b366ca0ca4022dc450a0d83a7a486d4
David Ko <david.ko@pvtmethod.com>
2017-06-10 08:34:09 -0400

    wip: map through item attributes

--- items/items.go
+++ items/items.go
@@ -46,8 +46,31 @@ func Seed(ddbSess *dynamodb.DynamoDB, ctx *cli.Context) {
 	ddbSess.BatchWriteItem(input)
 }
 
-func constructBatchWriteInput(tables interface{}) {
-  var itemInputs 
+func constructBatchWriteInput(tables interface{}) *dynamodb.BatchWriteItemInput {
+  var reqItems map[string][]*dynamodb.WriteRequest
 	for tableName, items := range tables.(map[string]interface{}) {
+    reqItems[tableName] = mapItemsToWriteRequests(items)
 	}
+  return &dynamodb.BatchWriteItemInput{
+    RequestItems: map[string][]*dynamodb.WriteRequest{
+    }
+  }
+}
+
+func mapItemsToWriteRequest(items interface{}) []*dynamodb.WriteRequest {
+  var writeReqs []*dynamodb.WriteRequest
+  for _, item := range items.([]map[string]string) {
+    writeReqs = append(writeReqs, &dynamodb.WriteRequest{
+      PutRequest: &dynamodb.PutRequest{
+        Item: mapItemToAttributeValues(item)
+      }
+    })
+  }
+}
+
+func mapItemToAttributeValues(item map[string]string) map[string]*dynamodb.AttributeValue{
+  var attrValues := map[string]*dynamodb.AttributeValue
+  for k, v := range item {
+    // attrValues[k] =
+  }
 }
